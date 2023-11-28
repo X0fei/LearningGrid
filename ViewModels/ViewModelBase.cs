3b70e282e5a208fd010c3896b0b1e1b91dfebbe9
Vasiliy <kimmiez@mail.ru>
2023-11-28 15:25:03 +0300

    v0.1

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace LearningGrid.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
+using System.Diagnostics;
+
+namespace LearningGrid.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
